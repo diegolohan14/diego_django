43c38bcf82096f4a701dc24051a38ae882fcce80
diegolohan14 <diegolohan14@gmail.com>
2025-04-02 21:16:26 -0300

    Começando o Projeto

--- APP/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Postagem
-
-class PostagemForm(forms.ModelForm):
-    class Meta:
-        model = Postagem
-        fields = ['titulo', 'conteudo']
--- diego_django/settings.py
+++ diego_django/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'APP',
-    'bootstrap4',
-    
+    'APP',    
 ]
 
 MIDDLEWARE = [
